3feb30ffb90504804918cf1c98001237e047a9c3
gnulitskaya <webgnule@mail.ru>
2023-12-08 15:17:29 +0300

    donut

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -6,13 +6,13 @@ import { easing } from 'maath'
 
 function Model({ children, color = 'white', roughness = 0, ...props }) {
     const ref = useRef()
-    const { nodes, materials } = useGLTF('/ball1.glb')
+    const { nodes, materials } = useGLTF('/donut.glb')
     useFrame((state, delta) => {
         easing.dampC(ref.current.material.color, color, 0.2, delta)
     })
     console.log(nodes, materials);
     return (
-        <mesh ref={ref} castShadow receiveShadow scale={1} geometry={nodes.Sphere.geometry}>
+        <mesh ref={ref} castShadow receiveShadow scale={2} geometry={nodes.Torus.geometry}>
             <meshStandardMaterial metalness={0.2} roughness={roughness} map={materials.map} />
             {children}
         </mesh>
